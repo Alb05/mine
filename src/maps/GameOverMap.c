e8c0904e2e36ab11a77e2954caa017ef3a72a626
Alb05 <cozzi.alberto05@gmail.com>
2022-09-27 20:49:50 +0200

    Updated tile source info

--- src/maps/GameOverMap.c
+++ src/maps/GameOverMap.c
@@ -8,7 +8,7 @@
    Section       : 
    Bank          : 0
    Map size      : 20 x 18
-   Tile set      : C:\Users\cozzi\Documents\gbdk\GBTD_GBMB_release\GBMB\tiles\MineTiles.gbr
+   Tile set      : C\assets\tiles\MineTiles.gbr
    Plane count   : 1 plane (8 bits)
    Plane order   : Tiles are continues
    Tile offset   : 0
--- src/maps/WinMap.h
+++ src/maps/WinMap.h
@@ -8,7 +8,7 @@
    Section       : 
    Bank          : 0
    Map size      : 4 x 1
-   Tile set      : C:\Users\cozzi\Documents\gbdk\GBTD_GBMB_release\GBMB\tiles\MineTiles.gbr
+   Tile set      : \assets\tiles\MineTiles.gbr
    Plane count   : 1 plane (8 bits)
    Plane order   : Tiles are continues
    Tile offset   : 0
